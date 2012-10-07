c093018f0fa429bc0121a01536f3a3adfb5b4bf3
Richard Joo <richardjoo@gmail.com>
2012-10-07 10:24:06 -0700

    Game.java updated

--- Game1/src/com/richardjoo/game1/Game.java
+++ Game1/src/com/richardjoo/game1/Game.java
@@ -1,7 +1,31 @@
 package com.richardjoo.game1;
 
-public class Game {
+public class Game implements Runnable {
 	public static int width = 300;
 	public static int height = ( width/16 ) * 9;  // this will make it to 16:9 screen ratio
 	public static int scale = 3; // how much our game would scaled up to. so, if it is 300 width, it would go up to 900
+	
+	// creating thread object so we can do multiple things simultaneously
+	private Thread thread;
+	
+	// we will create a new object to manipulate the thread
+	// synchronized here so we can prevent thread interferences and memory consistency errors
+	// without this, wrong thread management can lock down the system as well.
+	public synchronized void start() {
+		//so, this here is same as new Game()
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	// we need stop() to stop the thread too.
+	public synchronized void stop() {
+		try {
+			// join is used where the main thread to wait until all the threads are completed
+			thread.join();
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
