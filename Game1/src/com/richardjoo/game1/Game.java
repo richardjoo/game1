44c51cd0a658644ff4e238e513a8477bfed4d624
Richard Joo <richardjoo@gmail.com>
2012-10-06 21:29:33 -0700

    new files added

---
+++ Game1/src/com/richardjoo/game1/Game.java
@@ -0,0 +1,7 @@
+package com.richardjoo.game1;
+
+public class Game {
+	public static int width = 300;
+	public static int height = ( width/16 ) * 9;  // this will make it to 16:9 screen ratio
+	public static int scale = 3; // how much our game would scaled up to. so, if it is 300 width, it would go up to 900
+}
