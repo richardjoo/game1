1149d60279fe748a5764e1bce23b6e9fb40ae581
Richard Joo <richardjoo@gmail.com>
2012-10-10 11:32:40 -0700

    Game class updated. now it is at runnable

--- Game1/src/com/richardjoo/game1/Game.java
+++ Game1/src/com/richardjoo/game1/Game.java
@@ -1,17 +1,44 @@
 package com.richardjoo.game1;
 
-public class Game implements Runnable {
+import java.awt.Canvas;  // We need inherit Canvas class for our Game class
+import java.awt.Dimension; // Dimension class
+import javax.swing.JFrame;  // We will be using JFrame for our game window.
+
+
+public class Game extends Canvas implements Runnable {
+	
+	/**
+	 *	Setting default serialVersionUID 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	// setting up resolution
 	public static int width = 300;
 	public static int height = ( width/16 ) * 9;  // this will make it to 16:9 screen ratio
 	public static int scale = 3; // how much our game would scaled up to. so, if it is 300 width, it would go up to 900
 	
+	
 	// creating thread object so we can do multiple things simultaneously
 	private Thread thread;
+	private boolean running = false; // this will govern the entire game run or stop
+	private JFrame frame;
+	
+	
+	// Constructor
+	public Game() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
 	
 	// we will create a new object to manipulate the thread
 	// synchronized here so we can prevent thread interferences and memory consistency errors
 	// without this, wrong thread management can lock down the system as well.
 	public synchronized void start() {
+		// when the game starts, we set running to true so the game can keep on running
+		running = true;
+		
 		//so, this here is same as new Game()
 		thread = new Thread(this, "Display");
 		thread.start();
@@ -19,6 +46,8 @@ public class Game implements Runnable {
 	
 	// we need stop() to stop the thread too.
 	public synchronized void stop() {
+		// game stopping, meaning, we need to break out of while loop in run method
+		running = false;
 		try {
 			// join is used where the main thread to wait until all the threads are completed
 			// The join() method of a Thread instance can be used to "join" the start of a thread's execution to the end of another thread's execution so that a thread will not start running until another thread has ended. If join() is called on a Thread instance, the currently running thread will block until the Thread instance has finished executing.
@@ -27,6 +56,31 @@ public class Game implements Runnable {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	// since we are implementing Runnable, we need run method
+	public void run() {
+		// in here, we need to loop the game so the game won't stop and keep running
+		while (running) {
+			System.out.println("Running...");
+		}
+	}
+	
+	public static void main(String[] args) {
+		// instantiate Game
+		Game game = new Game();
+		
+		// set the frame
+		game.frame.setResizable(false);
+		game.frame.setTitle("Game1");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		// let's start the game!
+		game.start();
 	}
 	
 }
